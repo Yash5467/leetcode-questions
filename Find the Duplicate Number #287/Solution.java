0e2ac13d63f9444cd5418233a084754ce284b0a8
Yashpal Chouhan <yashstar0009@gmail.com>
2023-11-23 23:58:44 +0530

    Adding Find the Duplicate Number #287 solution

---
+++ Find the Duplicate Number #287/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+  public static void main(String[] args) {
+    int []nums={2,1,2};
+    System.out.println(findDuplicate(nums));
+  }
+
+  public static int findDuplicate(int[] nums) {
+    int i = 0;
+    while (i < nums.length) {
+      int correctIndex = nums[i] - 1;
+      if (nums[i] != nums[correctIndex]) {
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
+      } else i++;
+    }
+    for (int j = 0; j < nums.length; j++) {
+      if (nums[j] != (j + 1)) return nums[j];
+    }
+
+    return -1;
+  }
+}
